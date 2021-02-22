90101240f37cb1b106130e88876bd0e3775dfdbb
veronnah <yoo.veroon@gmail.com>
2021-02-22 21:15:44 +0100

    created calendar base

--- js/main.js
+++ js/main.js
@@ -1,7 +1,49 @@
+let today = new Date();
 
-    $('.slider').slick({
-        dots: true,
-        infinite: true,
-        prevArrow: '.arrow-prev',
-        nextArrow: '.arrow-next',
-    });
+let currentMonth = today.getMonth();
+let currentYear = today.getFullYear();
+
+let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+let month = document.querySelector('.month'); 
+
+showCalendar(currentMonth, currentYear);
+
+function showCalendar(month, year){
+    let firstDay = new Date(year, month).getDay(); 
+    
+    let daysInMonth = 32 - new Date(year, month, 32).getDate(); 
+    let calendarBody = document.querySelector('.cal__body'); 
+
+    calendarBody.innerHTML = months[month] + "" + year; 
+    
+    let date = 1; 
+
+    for(let i = 0; i < 6; i++){
+        let row = document.createElement('tr'); 
+
+        for(let j = 0; j < 7; j++){
+       
+            if(i === 0 && j < firstDay){
+                let cell = document.createElement('td');
+                let cellText = document.createTextNode(''); 
+                cell.appendChild(cellText); 
+                row.appendChild(cell);
+            }
+            else if(date < daysInMonth){
+                break;
+            }
+            else{
+                let cellText = document.createTextNode(date); 
+                cell.appendChild(cellText); 
+                row.appendChild(cell); 
+
+            }
+            date++; 
+        }
+
+        calendarBody.appendChild(row);
+
+    }
+}
+
