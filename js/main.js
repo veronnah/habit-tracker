f66902eb901e3d20ab25fe3c5342445a07b1ca43
veronnah <yoo.veroon@gmail.com>
2021-03-23 23:42:57 +0100

    daysOfMonth

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 let nav = 0; 
 
-
 const weekdays = [
     'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
     'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday',
@@ -11,10 +10,13 @@ const weekdays = [
 ];
 
 const checkBoxes = document.querySelector('.checkboxes__grid');
+let daysOfMonth = [];
+let rows = [];
 
 function load(){
     const dt = new Date(); 
-
+    rows =[];
+    daysOfMonth = [];
     if(nav !== 0){
         dt.setMonth(new Date().getMonth() + nav);
     }
@@ -31,8 +33,9 @@ function load(){
         month: 'numeric',
         day: 'numeric',
     });
+    console.log(dateString);
     const paddingDays = weekdays.indexOf(dateString.split(', ')[0]); 
-
+    
 
     document.querySelector('.monthDisplay').innerText = 
     `${dt.toLocaleDateString('en-us', {month: 'long'})} ${year}`; 
@@ -40,7 +43,7 @@ function load(){
     checkBoxes.innerHTML = ''; 
 
   
-    function createRows() {
+    function createRows(rowId) {
         for(let i = 1; i <= paddingDays + daysInMonth; i++){
            
             const checkboxRow = document.createElement('div');
@@ -53,25 +56,38 @@ function load(){
             </label>`; 
     
             const dayString = `${month + 1}/${i - paddingDays}/${year}`; 
-            
+            console.log();
+           
             if(i > paddingDays){
+                let ipadding = i - paddingDays;
                 checkboxRow.innerText = i - paddingDays;
+                let dayObj = {
+                    rowId: rowId,
+                    dayNumber: ipadding,
+                    dayName: new Date(dayString).toLocaleString('en-us', {weekday:'short'}),
+                };
+                daysOfMonth.push(dayObj);              
             }
             else{
                 checkboxRow.innerText = '*';
             }
+           
             checkBoxes.appendChild(checkboxRow);
         }
-    
+        return daysOfMonth;
     }
+    console.log(daysOfMonth);
     for(let i = 0; i < 5; i++){
-        createRows();
+         rows.push(createRows(i));
+         daysOfMonth = [];
     }
+    console.log(rows);
    
 }
 
 
 function navButtons(){
+  
     document.getElementById('prevBtn').addEventListener('click', ()=> {
         nav--; 
         load();
@@ -79,7 +95,9 @@ function navButtons(){
     document.getElementById('nextBtn').addEventListener('click', ()=> {
         nav++; 
         load();
+  
     });
+    
 }
 
 
